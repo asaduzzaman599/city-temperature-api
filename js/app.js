0df99ee7b6eeaf7c03cb2b1d0bdb0c703a7e4b04
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-03-02 21:48:51 +0600

    data fetched successfully

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-const API_KEY =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=c37f3551d980d0899f6e95a99a8a56c8&units=metric`
+const API_KEY ='c37f3551d980d0899f6e95a99a8a56c8';
+
+
+const searchTemperature =async () =>{
+    const cityName = document.getElementById('city-name').value;
+    if(cityName){
+        
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+        try{
+
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+            
+        }catch(e){
+            alert("Something is wrong.")
+        }
+    }
+
+}
