03f5122c786970ce7be158637227e0fa8dde2dd5
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-03-02 22:09:48 +0600

    done

--- js/app.js
+++ js/app.js
@@ -2,19 +2,38 @@ const API_KEY ='c37f3551d980d0899f6e95a99a8a56c8';
 
 
 const searchTemperature =async () =>{
-    const cityName = document.getElementById('city-name').value;
-    if(cityName){
+    const inputField = document.getElementById('city-name');
+    const cityName = inputField.value;
+    inputField.innerText= '';
+
         
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+        
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName?cityName:'london'}&appid=${API_KEY}&units=metric`;
         try{
 
             const res = await fetch(url);
             const data = await res.json();
             console.log(data);
+            displayWeatherCondition(data);
             
         }catch(e){
             alert("Something is wrong.")
         }
-    }
+    
 
 }
+
+const displayWeatherCondition = (weatherData) =>{
+    setInnerText('city',weatherData.name);
+    setInnerText('temperature',Math.round(weatherData.main.temp));
+    setInnerText('condition',weatherData.weather[0].main);
+
+    const iconUrl = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+    
+    document.getElementById('weather-icon').setAttribute('src',iconUrl);
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+searchTemperature();
